d442850a55e21995e4cab5210061bc197971d2a6
Joel Johnson <37419603+quantum-jo@users.noreply.github.com>
2020-12-30 10:32:27 +0530

    Fix: gallery router

--- routes/gallery.router.js
+++ routes/gallery.router.js
@@ -2,10 +2,6 @@ const router = require('express').Router();
 
 const galleryController = require('../controllers/gallery.controller');
 
-<<<<<<< HEAD
 router.get('/', galleryController.findAll);
-=======
-router.get('/',galleryController.findAll);
->>>>>>> 11fdf0b1708119177d3d8a2595f933600e1315ed
 
 module.exports = router;
